f1d5732fe9d1fd468a3d1aa5eabc90b0c2b6c80c
Quy Nguyen <quynguyen171297@gmail.com>
2022-08-30 18:20:20 +0700

    update #48

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import "./App.scss";
+import Header from "./components/Header/Header.js";
+import { Link } from "react-router-dom";
 
 const App = () => {
-  return <div className="app-container">hello world</div>;
+  return (
+    <div className="app-container">
+      <Header />
+      <div>
+        hello world
+        <div>
+          <button>
+            <Link to="/users">Go to user page</Link>
+          </button>
+          <button>
+            <Link to="/admins">Go to admin page</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default App;
