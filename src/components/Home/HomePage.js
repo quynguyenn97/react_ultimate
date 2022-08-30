4377397670bc609baa43fd4ab81d9d6a748607c7
Quy Nguyen <quynguyen171297@gmail.com>
2022-08-30 23:05:31 +0700

    #51

---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,5 @@
+const HomePage = (props) => {
+  return <div>HomePage ne ku</div>;
+};
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import User from "./components/User/User";
 import Admin from "./components/Admin/Admin";
+import HomePage from "./components/Home/HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,9 +16,11 @@ root.render(
     {/* <React.StrictMode> */}
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="users" element={<User />} />
-        <Route path="admins" element={<Admin />} />
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="users" element={<User />} />
+          <Route path="admins" element={<Admin />} />
+        </Route>
       </Routes>
     </BrowserRouter>
     {/* </React.StrictMode> */}
